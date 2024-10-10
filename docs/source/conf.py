08aedd5e6244f29ab34c9463e4fca2d61b5107f7
brobeson <brobeson@users.noreply.github.com>
2024-10-10 13:54:34 -0600

    Tweak more docs configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,15 +1,20 @@
-# Configuration file for the Sphinx documentation builder.
-#
+"""Configuration file for the Sphinx documentation builder."""
+
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# pylint: disable=invalid-name
+# cspell: words furo
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "PBench"
-copyright = "2024, brobeson"
+project_copyright = "2024, brobeson"
 author = "brobeson"
+version = "0.0.0"
 release = "0.0.0"
+highlight_language = "python"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -32,10 +37,8 @@ html_theme_options = {
     # "dark_css_variables"
     # "sidebar_hide_name": True,     # Keep the name in the side bar
     # "navigation_with_keys": True,  # Don't see this doing anything
-    "top_of_page_buttons": ["edit", "view"],
-    "source_repository": "https://github.com/brobeson/PBench",
-    "source_branch": "14-configure-read-the-docs-project",
-    "source_directory": "docs/source/",
+    # "top_of_page_buttons": ["edit", "view"],
+    # "source_repository": "https://github.com/brobeson/PBench",
+    # "source_branch": "14-configure-read-the-docs-project",
+    # "source_directory": "docs/source/",
 }
-
-# TODO Set a logo. May need separate light & dark logos.
